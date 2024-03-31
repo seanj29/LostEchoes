4bf4ffe4e3f1cafff39e7dbe31ae62d4ab2234f6
Sean, Otto <seanotto29@gmail.com>
2024-03-31 22:41:20 +0100

    changed movement to 8 way and removed collision from base tilemap tile

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -1,21 +1,19 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var SPEED = 300.0
+@export var FRICTION = 1500.00
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
 	move_and_slide()

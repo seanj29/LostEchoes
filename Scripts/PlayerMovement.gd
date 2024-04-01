0c68046db669bb6eb8920bf58bd98705ec9d22b0
Sean, Otto <seanotto29@gmail.com>
2024-04-01 01:41:24 +0100

    Adde basics in relation to replay script, along with replay frame internal class

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -4,12 +4,15 @@ extends CharacterBody2D
 @export var SPEED = 300.0
 @export var FRICTION = 1500.00
 
+var ReplayTest:Array[ReplayFrame] = []
 
+func _physics_process(_delta):
 
-func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
+	# TODO add better actions blah blah
+
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if direction:
 		velocity = direction * SPEED
@@ -17,3 +20,23 @@ func _physics_process(delta):
 		velocity = Vector2.ZERO
 
 	move_and_slide()
+
+
+func _unhandled_key_input(event: InputEvent):
+
+	if event.is_action_pressed("ui_up"):
+		var FrameObject = ReplayFrame.new(Engine.get_physics_frames(), ["Move UP"])
+		ReplayTest.append(FrameObject)
+		print("FrameList:")
+		for Frame in ReplayTest:
+			print("	Frame: %s 	Actions: %s"  % [Frame.FrameNumber, Frame.ActionArr])	
+
+
+class ReplayFrame:
+
+	var FrameNumber: int
+	var ActionArr: Array[String]
+
+	func _init(Frame: int, Actions: Array[String]):
+		FrameNumber = Frame
+		ActionArr = Actions	
---
+++ Scripts/RepalysScript.gd
@@ -0,0 +1,5 @@
+extends Resource
+
+# Array with (Frame, array of actions/inputs, currentState) 
+# Could even make an object called "Frame" which has a number, array of actions and playerstate variables?
+

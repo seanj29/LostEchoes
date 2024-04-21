80bb2b4092a993b1e6906176d4b1c3c557cc32e2
Sean, Otto <seanotto29@gmail.com>
2024-04-21 11:19:51 +0100

    oops

--- Scripts/Utils/FSM/States/attack_anim_state.gd
+++ Scripts/Utils/FSM/States/attack_anim_state.gd
@@ -2,6 +2,7 @@ class_name AttackAnimState
 extends AnimState
 
 func Enter():
+	super()
 	if sprite.animation.begins_with("Attack"):
 		if not sprite.animation_finished.is_connected(change):
 			sprite.animation_finished.connect(change)
